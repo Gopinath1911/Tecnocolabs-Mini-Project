5f62d4b782f377c97b4eec103ae2666011d1408d
Gopinath1911 <72126553+Gopinath1911@users.noreply.github.com>
2020-12-08 23:15:33 +0530

    Add files via upload

--- Toxic_comment_classification.py
+++ Toxic_comment_classification.py
@@ -1,39 +1,159 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[18]:
 
 
 import pandas as pd 
-
-df=pd.read_csv("train.csv")
-
-
-# In[2]:
+import numpy as np
+train=pd.read_csv("train.csv")
+test=pd.read_csv("test.csv")
 
 
-df.head()
+# In[20]:
 
 
-# In[9]:
+import gc
+import time
+import warnings
+
+#stats
+
+from scipy import sparse
+import scipy.stats as ss
+
+#viz
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec 
+import seaborn as sns
+from wordcloud import WordCloud ,STOPWORDS
+from PIL import Image
+import matplotlib_venn as venn
+
+#nlp
+import string
+import re    #for regex
+import nltk
+from nltk.corpus import stopwords
+import spacy
+from nltk import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer 
+from nltk.tokenize import word_tokenize
+# Tweet tokenizer does not split at apostophes which is what we want
+from nltk.tokenize import TweetTokenizer   
 
 
-def gen_freq(text):
-    #Will store the list of words
-    word_list = []
+#FeatureEngineering
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.metrics import log_loss
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
 
-    #Loop over all the tweets and extract words into word_list
-    for tw_words in text.split():
-        word_list.extend(tw_words)
 
-    #Create word frequencies using word_list
-    word_freq = pd.Series(word_list).value_counts()
 
-    print(word_freq[:20])
-    
-    return word_freq
 
-gen_freq(df.comment_text.str)
+
+#settings
+start_time=time.time()
+color = sns.color_palette()
+sns.set_style("dark")
+eng_stopwords = set(stopwords.words("english"))
+warnings.filterwarnings("ignore")
+
+lem = WordNetLemmatizer()
+tokenizer=TweetTokenizer()
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[ ]:
+
+
+train.head(10)
+
+
+# In[ ]:
+
+
+nrows_train=train.shape[0]
+nrows_test=test.shape[0]
+sum=nrows_train+nrows_test
+
+print("     : train  : test")
+print("rows :", nrows_train, ":", nrows_test)
+print("perc :", round(nrows_train*100/sum), "    :", round(nrows_test*100/sum))
+
+
+# In[ ]:
+
+
+x=train.iloc[:,2:].sum()
+rowsum=train.iloc[:,2:].sum(axis=1)
+train['clean']=(rowsum==0)
+train['clean'].sum()
+print('Total comments = ', len(train))
+print('Total clean comments =', train['clean'].sum())
+print('Total tags =', x.sum())
+
+
+# In[ ]:
+
+
+x=train.iloc[:,2:].sum()
+#plot
+plt.figure(figsize=(8,4))
+ax= sns.barplot(x.index, x.values, alpha=0.8)
+plt.title("# per class")
+plt.ylabel('# of Occurrences', fontsize=12)
+plt.xlabel('Type ', fontsize=12)
+#adding the text labels
+rects = ax.patches
+labels = x.values
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width()/2, height + 5, label, ha='center', va='bottom')
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+#plot
+plt.figure(figsize=(8,4))
+ax = sns.barplot(x.index, x.values, alpha=0.8,color=color[2])
+plt.title("Multiple tags per comment")
+plt.ylabel('# of Occurrences', fontsize=12)
+plt.xlabel('# of tags ', fontsize=12)
+
+#adding the text labels
+rects = ax.patches
+labels = x.values
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width()/2, height + 5, label, ha='center', va='bottom')
+
+plt.show()
+
+
+# In[ ]:
+
+
+temp_df=train.iloc[:,2:-1]
+# filter temp by removing clean comments
+# temp_df=temp_df[~train.clean]
+
+corr=temp_df.corr()
+plt.figure(figsize=(10,8))
+sns.heatmap(corr,
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values, annot=True)
 
 
 # In[ ]:
