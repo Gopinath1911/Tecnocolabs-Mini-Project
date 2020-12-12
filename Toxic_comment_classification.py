0b0e756d82ab9f841bdf6b7c872b94e138f68f07
Gopinath1911 <72126553+Gopinath1911@users.noreply.github.com>
2020-12-12 22:41:18 +0530

    Add files via upload

--- Toxic_comment_classification.py
+++ Toxic_comment_classification.py
@@ -10,7 +10,7 @@ train=pd.read_csv("train.csv")
 test=pd.read_csv("test.csv")
 
 
-# In[20]:
+# In[2]:
 
 
 import gc
@@ -67,66 +67,52 @@ tokenizer=TweetTokenizer()
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[21]:
+# In[3]:
 
 
 train.head()
 
 
-# In[22]:
+# In[4]:
 
 
 test.head()
 
 
-# In[24]:
+# In[5]:
 
 
 train.isnull().sum()
 
 
-# In[25]:
+# In[6]:
 
 
 test.isnull().sum()
 
 
-# In[35]:
+# In[7]:
 
 
 train.info()
 
 
-# In[37]:
+# In[8]:
 
 
 test.info()
 
 
-# In[55]:
+# In[9]:
 
 
-train['no_tag']=train.iloc[:,2:].sum(axis=1)
-train.no_tag.value_counts()
+row_sums=train.iloc[:,2:].sum(axis=1)
+x=row_sums.value_counts()
+print(x)
+train['clean']=(row_sums==0)
 
 
-# In[58]:
-
-
-train.no_tag.value(42)
-
-
-# In[42]:
-
-
-no_tag=train.iloc[:,2:].sum(axis=1)
-Train['clean']=1 
-if (no_tag==0): 
-else: 
-(0)
-
-
-# In[33]:
+# In[10]:
 
 
 x=train.iloc[:,2:].sum()
@@ -145,6 +131,432 @@ for rect, label in zip(rects, labels):
     ax.text(rect.get_x() + rect.get_width()/2, height + 5, label, ha="center", va='bottom')
 
 plt.show()
+
+
+# In[11]:
+
+
+def gen_freq(text):
+    #Will store the list of words
+    word_list = []
+
+    #Loop over all the tweets and extract words into word_list
+    for tw_words in text.split():
+        word_list.extend(tw_words)
+
+    #Create word frequencies using word_list
+    word_freq = pd.Series(word_list).value_counts()
+
+    
+    return word_freq
+
+gen_freq(train.comment_text.str)
+
+
+# In[12]:
+
+
+word_freq = gen_freq(train.comment_text.str)
+
+#Generate word cloud
+wc = WordCloud(width=400, height=330, max_words=100, background_color='white').generate_from_frequencies(word_freq)
+
+plt.figure(figsize=(12, 8))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+
+# In[13]:
+
+
+merge=pd.concat([train.iloc[:,0:2],test.iloc[:,0:2]])
+df=merge.reset_index(drop=True)
+
+
+# In[14]:
+
+
+df['count_sent']=df["comment_text"].apply(lambda x: len(re.findall("\n",str(x)))+1)
+#Word count in each comment:
+df['count_word']=df["comment_text"].apply(lambda x: len(str(x).split()))
+#Unique word count
+df['count_unique_word']=df["comment_text"].apply(lambda x: len(set(str(x).split())))
+#Letter count
+df['count_letters']=df["comment_text"].apply(lambda x: len(str(x)))
+#punctuation count
+df["count_punctuations"] =df["comment_text"].apply(lambda x: len([c for c in str(x) if c in string.punctuation]))
+#upper case words count
+df["count_words_upper"] = df["comment_text"].apply(lambda x: len([w for w in str(x).split() if w.isupper()]))
+#title case words count
+df["count_words_title"] = df["comment_text"].apply(lambda x: len([w for w in str(x).split() if w.istitle()]))
+#Number of stopwords
+df["count_stopwords"] = df["comment_text"].apply(lambda x: len([w for w in str(x).lower().split() if w in eng_stopwords]))
+#Average length of the words
+df["mean_word_len"] = df["comment_text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+
+
+# In[15]:
+
+
+df['word_unique_percent']=df['count_unique_word']*100/df['count_word']
+#derived features
+#Punct percent in each comment:
+df['punct_percent']=df['count_punctuations']*100/df['count_word']
+
+
+# In[16]:
+
+
+#serperate train and test features
+train_feats=df.iloc[0:len(train),]
+test_feats=df.iloc[len(train):,]
+#join the tags
+train_tags=train.iloc[:,2:]
+train_feats=pd.concat([train_feats,train_tags],axis=1)
+
+
+# In[17]:
+
+
+train_feats['count_sent'].loc[train_feats['count_sent']>10] = 10 
+plt.figure(figsize=(12,6))
+## sentenses
+plt.subplot(121)
+sns.boxplot(y='count_sent',x='clean', data=train_feats)
+plt.xlabel('Clean', fontsize=12)
+plt.ylabel('# of sentences', fontsize=12)
+plt.title("Number of sentences in each comment", fontsize=15)
+# words
+train_feats['count_word'].loc[train_feats['count_word']>200] = 200
+plt.subplot(122)
+sns.boxplot(y='count_word',x='clean', data=train_feats)
+plt.xlabel('Clean', fontsize=12)
+plt.ylabel('# of words', fontsize=12)
+plt.title("Number of words in each comment", fontsize=15)
+
+plt.show()
+
+
+# In[18]:
+
+
+corpus=merge.comment_text
+
+
+# In[19]:
+
+
+APPO = {
+"aren't" : "are not",
+"can't" : "cannot",
+"couldn't" : "could not",
+"didn't" : "did not",
+"doesn't" : "does not",
+"don't" : "do not",
+"hadn't" : "had not",
+"hasn't" : "has not",
+"haven't" : "have not",
+"he'd" : "he would",
+"he'll" : "he will",
+"he's" : "he is",
+"i'd" : "I would",
+"i'd" : "I had",
+"i'll" : "I will",
+"i'm" : "I am",
+"isn't" : "is not",
+"it's" : "it is",
+"it'll":"it will",
+"i've" : "I have",
+"let's" : "let us",
+"mightn't" : "might not",
+"mustn't" : "must not",
+"shan't" : "shall not",
+"she'd" : "she would",
+"she'll" : "she will",
+"she's" : "she is",
+"shouldn't" : "should not",
+"that's" : "that is",
+"there's" : "there is",
+"they'd" : "they would",
+"they'll" : "they will",
+"they're" : "they are",
+"they've" : "they have",
+"we'd" : "we would",
+"we're" : "we are",
+"weren't" : "were not",
+"we've" : "we have",
+"what'll" : "what will",
+"what're" : "what are",
+"what's" : "what is",
+"what've" : "what have",
+"where's" : "where is",
+"who'd" : "who would",
+"who'll" : "who will",
+"who're" : "who are",
+"who's" : "who is",
+"who've" : "who have",
+"won't" : "will not",
+"wouldn't" : "would not",
+"you'd" : "you would",
+"you'll" : "you will",
+"you're" : "you are",
+"you've" : "you have",
+"'re": " are",
+"wasn't": "was not",
+"we'll":" will",
+"didn't": "did not",
+"tryin'":"trying"
+}
+
+
+# In[20]:
+
+
+def clean(comment):
+        
+    comment=comment.lower()
+    #remove \n
+    comment=re.sub("\\n","",comment)
+    # remove leaky elements like ip,user
+    comment=re.sub("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}","",comment)
+    #removing usernames
+    comment=re.sub("\[\[.*\]","",comment)
+    
+    #Split the sentences into words
+    words=tokenizer.tokenize(comment)
+    
+    # (')aphostophe  replacement (ie)   you're --> you are  
+    # ( basic dictionary lookup : master dictionary present in a hidden block of code)
+    words=[APPO[word] if word in APPO else word for word in words]
+    
+    words=[lem.lemmatize(word, "v") for word in words]
+    
+    words = [w for w in words if not w in eng_stopwords]
+    
+    clean_sent=" ".join(words)
+    # remove any non alphanum,digit character
+    #clean_sent=re.sub("\W+"," ",clean_sent)
+    #clean_sent=re.sub("  "," ",clean_sent)
+    return(clean_sent)
+
+
+# In[21]:
+
+
+clean_corpus=corpus.apply(lambda x :clean(x))
+
+
+# In[33]:
+
+
+tfv = TfidfVectorizer(min_df=200,  max_features=10000, 
+            strip_accents='unicode', analyzer='word',ngram_range=(1,1),
+            use_idf=1,smooth_idf=1,sublinear_tf=1,
+            stop_words = 'english')
+tfv.fit(clean_corpus)
+features = np.array(tfv.get_feature_names())
+
+train_unigrams =  tfv.transform(clean_corpus.iloc[:train.shape[0]])
+test_unigrams = tfv.transform(clean_corpus.iloc[train.shape[0]:])
+
+
+# In[34]:
+
+
+tfv = TfidfVectorizer(min_df=150,  max_features=30000, 
+            strip_accents='unicode', analyzer='word',ngram_range=(2,2),
+            use_idf=1,smooth_idf=1,sublinear_tf=1,
+            stop_words = 'english')
+
+tfv.fit(clean_corpus)
+features = np.array(tfv.get_feature_names())
+train_bigrams =  tfv.transform(clean_corpus.iloc[:train.shape[0]])
+test_bigrams = tfv.transform(clean_corpus.iloc[train.shape[0]:])
+#get top n for bigrams
+tfidf_top_n_per_lass=top_feats_by_class(train_bigrams,features)
+
+
+# In[36]:
+
+
+tfv = TfidfVectorizer(min_df=100,  max_features=30000, 
+            strip_accents='unicode', analyzer='char',ngram_range=(1,4),
+            use_idf=1,smooth_idf=1,sublinear_tf=1,
+            stop_words = 'english')
+
+tfv.fit(clean_corpus)
+features = np.array(tfv.get_feature_names())
+train_charngrams =  tfv.transform(clean_corpus.iloc[:train.shape[0]])
+test_charngrams = tfv.transform(clean_corpus.iloc[train.shape[0]:])
+end_time=time.time()
+print("total time till charngrams",end_time-start_time)
+
+
+# In[23]:
+
+
+def top_tfidf_feats(row, features, top_n=25):
+    ''' Get top n tfidf values in row and return them with their corresponding feature names.'''
+    topn_ids = np.argsort(row)[::-1][:top_n]
+    top_feats = [(features[i], row[i]) for i in topn_ids]
+    df = pd.DataFrame(top_feats)
+    df.columns = ['feature', 'tfidf']
+    return df
+
+def top_feats_in_doc(Xtr, features, row_id, top_n=25):
+    ''' Top tfidf features in specific document (matrix row) '''
+    row = np.squeeze(Xtr[row_id].toarray())
+    return top_tfidf_feats(row, features, top_n)
+
+def top_mean_feats(Xtr, features, grp_ids, min_tfidf=0.1, top_n=25):
+    ''' Return the top n features that on average are most important amongst documents in rows
+        indentified by indices in grp_ids. '''
+    
+    D = Xtr[grp_ids].toarray()
+    D[D < min_tfidf] = 0
+    tfidf_means = np.mean(D, axis=0)
+    return top_tfidf_feats(tfidf_means, features, top_n)
+
+# modified for multilabel milticlass
+def top_feats_by_class(Xtr, features, min_tfidf=0.1, top_n=20):
+    ''' Return a list of dfs, where each df holds top_n features and their mean tfidf value
+        calculated across documents with the same class label. '''
+    dfs = []
+    cols=train_tags.columns
+    for col in cols:
+        ids = train_tags.index[train_tags[col]==1]
+        feats_df = top_mean_feats(Xtr, features, ids, min_tfidf=min_tfidf, top_n=top_n)
+        feats_df.label = label
+        dfs.append(feats_df)
+    return dfs
+
+
+# In[26]:
+
+
+tfidf_top_n_per_lass=top_feats_by_class(train_unigrams,features)
+
+
+# In[27]:
+
+
+class NbSvmClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, C=1.0, dual=False, n_jobs=1):
+        self.C = C
+        self.dual = dual
+        self.n_jobs = n_jobs
+
+    def predict(self, x):
+        # Verify that model has been fit
+        check_is_fitted(self, ['_r', '_clf'])
+        return self._clf.predict(x.multiply(self._r))
+
+    def predict_proba(self, x):
+        # Verify that model has been fit
+        check_is_fitted(self, ['_r', '_clf'])
+        return self._clf.predict_proba(x.multiply(self._r))
+
+    def fit(self, x, y):
+        # Check that X and y have correct shape
+        y = y.values
+        x, y = check_X_y(x, y, accept_sparse=True)
+
+        def pr(x, y_i, y):
+            p = x[y==y_i].sum(0)
+            return (p+1) / ((y==y_i).sum()+1)
+
+        self._r = sparse.csr_matrix(np.log(pr(x,1,y) / pr(x,0,y)))
+        x_nb = x.multiply(self._r)
+        self._clf = LogisticRegression(C=self.C, dual=self.dual, n_jobs=self.n_jobs).fit(x_nb, y)
+        return self
+
+
+# In[28]:
+
+
+SELECTED_COLS=['count_sent', 'count_word', 'count_unique_word',
+       'count_letters', 'count_punctuations', 'count_words_upper',
+       'count_words_title', 'count_stopwords', 'mean_word_len',
+       'word_unique_percent', 'punct_percent']
+target_x=train_feats[SELECTED_COLS]
+# target_x
+
+TARGET_COLS=['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+target_y=train_tags[TARGET_COLS]
+
+
+# In[29]:
+
+
+#Just the indirect features -- meta features
+print("Using only Indirect features")
+model = LogisticRegression(C=3)
+X_train, X_valid, y_train, y_valid = train_test_split(target_x, target_y, test_size=0.33, random_state=2018)
+train_loss = []
+valid_loss = []
+importance=[]
+preds_train = np.zeros((X_train.shape[0], len(y_train)))
+preds_valid = np.zeros((X_valid.shape[0], len(y_valid)))
+for i, j in enumerate(TARGET_COLS):
+    print('Class:= '+j)
+    model.fit(X_train,y_train[j])
+    preds_valid[:,i] = model.predict_proba(X_valid)[:,1]
+    preds_train[:,i] = model.predict_proba(X_train)[:,1]
+    train_loss_class=log_loss(y_train[j],preds_train[:,i])
+    valid_loss_class=log_loss(y_valid[j],preds_valid[:,i])
+    print('Trainloss=log loss:', train_loss_class)
+    print('Validloss=log loss:', valid_loss_class)
+    importance.append(model.coef_)
+    train_loss.append(train_loss_class)
+    valid_loss.append(valid_loss_class)
+print('mean column-wise log loss:Train dataset', np.mean(train_loss))
+print('mean column-wise log loss:Validation dataset', np.mean(valid_loss))
+
+end_time=time.time()
+print("total time till Indirect feat model",end_time-start_time)
+
+
+# In[37]:
+
+
+from scipy.sparse import csr_matrix, hstack
+
+#Using all direct features
+print("Using all features except leaky ones")
+target_x = hstack((train_bigrams,train_charngrams,train_unigrams,train_feats[SELECTED_COLS])).tocsr()
+
+
+end_time=time.time()
+print("total time till Sparse mat creation",end_time-start_time)
+
+
+# In[38]:
+
+
+model = NbSvmClassifier(C=4, dual=True, n_jobs=-1)
+X_train, X_valid, y_train, y_valid = train_test_split(target_x, target_y, test_size=0.33, random_state=2018)
+train_loss = []
+valid_loss = []
+preds_train = np.zeros((X_train.shape[0], len(y_train)))
+preds_valid = np.zeros((X_valid.shape[0], len(y_valid)))
+for i, j in enumerate(TARGET_COLS):
+    print('Class:= '+j)
+    model.fit(X_train,y_train[j])
+    preds_valid[:,i] = model.predict_proba(X_valid)[:,1]
+    preds_train[:,i] = model.predict_proba(X_train)[:,1]
+    train_loss_class=log_loss(y_train[j],preds_train[:,i])
+    valid_loss_class=log_loss(y_valid[j],preds_valid[:,i])
+    print('Trainloss=log loss:', train_loss_class)
+    print('Validloss=log loss:', valid_loss_class)
+    train_loss.append(train_loss_class)
+    valid_loss.append(valid_loss_class)
+print('mean column-wise log loss:Train dataset', np.mean(train_loss))
+print('mean column-wise log loss:Validation dataset', np.mean(valid_loss))
+
+
+end_time=time.time()
+print("total time till NB base model creation",end_time-start_time)
 
 
 # In[ ]:
